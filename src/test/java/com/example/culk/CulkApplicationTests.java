8a9df51a32ccebae1f08d1ffa4baf0674b0e3651
Levan244 <panlevan9@gmail.com>
2023-07-26 13:11:11 +0400

    Init commit

---
+++ src/test/java/com/example/culk/CulkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.culk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
